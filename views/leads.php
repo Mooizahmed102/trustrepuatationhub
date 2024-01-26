a384759b89987ee71388c4ad84a59b3e73e071bc
moizahmed <amoiz6263@gmail.com>
2024-01-27 04:01:45 +0500

    first commit

--- views/leads.php
+++ views/leads.php
@@ -1,17 +1,5 @@
 <?php
 
-require "vendor/autoload.php";
-
-$robo = 'info@Trustreputationhub.com';
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-
-if($_SERVER['REQUEST_METHOD'] != "POST"){
-    die("This request method is not supported for this request");
-}
-
 if(!isset($_REQUEST['name']) ||
 !isset($_REQUEST['email'])||
 !isset($_REQUEST['phone']) ||
@@ -24,15 +12,17 @@ $name = $_REQUEST['name'];
 $email = $_REQUEST['email'];
 $phone = $_REQUEST['phone'];
 $brief = $_REQUEST['brief'];
+       
+$to = "info@trustreputationhub.com";
+$subject = "LEAD";
 
-//lead work here 
 $message = "<h1>Request to start</h1>";
 $message.= "<table width='100%' border='1'>";
 $message.= "<tbody>";
 $message.= "<tr><td colspan='5' align='center' style='font-size: 25px;font-weight: 800;'>User Details</td></tr>";
 $message.= "<tr>
             <th>Name</th>
-            <th>Email</th>
+            <th>email</th>
             <th>Phone</th>
             <th>brief</th>
         </tr>";
@@ -44,44 +34,16 @@ $message.= "<tr>
         </tr>";
 $message.= "</tbody>";
 $message.= "</table>";
-$developmentMode = true;
-$mailer = new PHPMailer($developmentMode);
 
-try {
-    $mailer->SMTPDebug = 2;
-    $mailer->isSMTP();
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-    if ($developmentMode) {
-    $mailer->SMTPOptions = [
-        'ssl'=> [
-        'verify_peer' => false,
-        'verify_peer_name' => false,
-        'allow_self_signed' => true
-        ]
-    ];
-    }
+// More headers
+$headers .= 'From: <info@trustreputationhub.com>' . "\r\n";
+$headers .= 'Cc: info@trustreputationhub.com' . "\r\n";
 
-
-    $mailer->Host = 'smtp.gmail.com';
-    $mailer->SMTPAuth = true;
-    $mailer->Username = 'info@Trustreputationhub.com';
-    $mailer->Password = 'hlbdbgjzuhymrxnf';
-    $mailer->SMTPSecure = 'ssl';
-    $mailer->Port = 465;
-
-    $mailer->setFrom('info@Trustreputationhub.com', 'Sign Up Lead');
-    $mailer->addAddress('info@Trustreputationhub.com', 'Query Alert');
-
-    $mailer->isHTML(true);
-    $mailer->Subject = 'Query';
-    $mailer->Body = $message;
-
-    $mailer->send();
-    $mailer->ClearAllRecipients();
-    echo "MAIL HAS BEEN SENT SUCCESSFULLY";
+mail($to,$subject,$message,$headers);
     header("location:/thank-you/");
     
-
-} catch (Exception $e) {
-    echo "EMAIL SENDING FAILED. INFO: " . $mailer->ErrorInfo;
-}
+?>
---
+++ views/thank-you.php
@@ -0,0 +1,257 @@
+
+<!-- BEGIN: HEADER -->
+<!DOCTYPE html>
+<html>
+
+<!-- Mirrored from www.TrustReputationHub.com/contact by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 13 Jan 2024 22:36:07 GMT -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<?php include_once('includes/head.php')?>
+<body>
+<?php include_once('includes/header.php')?></body>
+
+</body>
+
+<!-- Mirrored from www.TrustReputationHub.com/contact by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 13 Jan 2024 22:36:07 GMT -->
+</html>
+
+<section class="inner-banner contact-banner">
+<img alt="Image" class="img-responsive" src="img/insight-banner.jpg">
+<div class="banner-text">
+<div class="container">
+<div class="row ">
+<div class="col-md-10 col-sm-10 col-xs-12 center-col">
+<h3>Thankyou</h3>
+<p>Thankyou for your response we will contact you shortly</p>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+<section class="contact-section bottom-section">
+<div class="container">
+<div class="row">
+<div class="col-md-6 col-sm-6 col-xs-12">
+<div class="contact-left">
+<h3 class="section-head"><span>Get in Touch</span> With Us</h3>
+
+<div class="row">
+<div class="col-md-6 col-sm-6 col-xs-12">
+
+<div class="contact-box">
+<h4><i class="fa fa-phone" aria-hidden="true"></i> Call Us 24/7</h4>
+<p><a href="tel:(855) 364-3354">(855) 364-3354</a></p>
+</div>
+<div class="contact-box">
+<h4><i class="fa fa-envelope-o" aria-hidden="true"></i> Email Us</h4>
+<p><a href="mailto:info@Trustreputationhub.com">info@Trustreputationhub.com</a></p>
+</div>
+
+</div>
+
+</div>
+</div>
+</div>
+<div class="col-md-6 col-sm-6 col-xs-12">
+<div class="contact-left">
+<h3 class="section-head">Leave Message</h3>
+<form method="POST" class="contact-form" action="/leads/">
+<input type="hidden" value="2" name="contactform">
+<div class="col-md-12 nopadding">
+<div class="form-group">
+<i class="fa fa-user" aria-hidden="true"></i>
+<input class="form-control" placeholder="Full Name" name="name" required type="text">
+</div>
+</div>
+<div class="col-md-12 nopadding">
+<div class="form-group">
+<i class="fa fa-envelope" aria-hidden="true"></i>
+<input class="form-control" name="email" placeholder="Email" required type="email">
+</div>
+</div>
+<div class="col-md-12 nopadding">
+<div class="form-group">
+<i class="fa fa-phone" aria-hidden="true"></i>
+<input class="form-control" name="phone" placeholder="Phone Number" onkeypress="if (!window.__cfRLUnblockHandlers) return false; return event.charCode >= 48 && event.charCode <= 57" required type="phone" data-cf-modified-e92913b86c099b3eaedf0d66->
+</div>
+</div>
+<div class="col-md-12 nopadding">
+<div class="form-group">
+<i class="fa fa-comment" aria-hidden="true"></i>
+<textarea class="form-control" name="brief" placeholder="Your project details here" rows="8"></textarea>
+</div>
+</div>
+<div class="row">
+<div class="col-md-12">
+<div class="form-group">
+<button type="submit" class="blue-btn hvr-bounce-to-right" type="buton">Submit</button>
+<div class="control-group clearfix  ">
+<input type="hidden" id="lead_area" name="lead_area" value="for $179  ">
+<input type="hidden" id="lead_org_price" name="lead_org_price" value>
+<input type="hidden" name="send" value="1">
+
+<input type="hidden" name="lb_source" value>
+<input type="hidden" name="lb_source_cat" value>
+<input type="hidden" name="lb_source_nam" value>
+<input type="hidden" name="lb_source_ema" value>
+<input type="hidden" name="lb_source_con" value>
+<input type="hidden" name="lb_source_pho" value>
+<input type="hidden" name="lb_source_off" value>
+
+<input type="hidden" name="fullpageurl" value>
+<input type="hidden" name="pageurl" value="TrustReputationHub.com/">
+
+<input type="hidden" name="ip2loc_ip" value="162.158.166.238">
+<input type="hidden" name="ip2loc_isp" value="Cloudflare, Inc.">
+<input type="hidden" name="ip2loc_org" value="Cloudflare, Inc.">
+<input type="hidden" name="ip2loc_country" value="Singapore">
+<input type="hidden" name="ip2loc_region" value>
+<input type="hidden" name="ip2loc_city" value="Singapore">
+<input type="hidden" name="service_id" value="5731" class="service_id">
+</div>
+</div>
+</div>
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+<div class="contact_page">
+<section class="contact-section">
+<div class="container">
+<div class="row text-center">
+<div class="col-md-8 col-sm-8 col-xs-12 center-col">
+<h3 class="section-head wow bounceIn" data-wow-delay="0.6s" data-wow-duration="2s">Let's discuss your next project
+<a href="contact/">CONNECT NOW</a></h3>
+</div>
+</div>
+</div>
+<div class="container-fluid">
+<div class="row">
+<div class="col-md-6 col-sm-6 col-xs-12 nopadding blue-bg">
+<div class="expert-part wow fadeInLeft" data-wow-delay="0.6s" data-wow-duration="2s">
+<h4>We’ve driven over 6,271,747 leads for our clients. Discover how we can help your business grow.</h4>
+<form method="POST" data-noinfo="true" class=" exp-form" action="/leads/">
+<input type="hidden" value="1" name="contactform">
+<div class="col-md-6 ">
+<div class="form-group">
+<input class="form-control" name="name" placeholder="Full Name" required type="text">
+</div>
+</div>
+<div class="col-md-6 ">
+<div class="form-group">
+<input class="form-control" name="company_name" placeholder="Company Name" required type="text">
+</div>
+</div>
+<div class="col-md-6 ">
+<div class="form-group">
+<input class="form-control" name="email" placeholder="Email Address" required type="email">
+</div>
+</div>
+<div class="col-md-6 ">
+<div class="form-group">
+<input class="form-control" name="phone" placeholder="Phone Number" required type="phone">
+</div>
+</div>
+<div class="col-md-12 ">
+<p class="select-note">Please Select Your Service</p>
+<div class="form-group">
+<select id="sort" name="select_services" required>
+<option value> Select Services </option>
+<option value="Logo Design"> Logo Design </option>
+<option value="Branding"> Branding</option>
+<option value="Ecommerce Websites">Ecommerce Websites</option>
+<option value="Domian & Hosting">Domian & Hosting</option>
+<option value="Marketing">Marketing</option>
+<option value="Website Maintenance">Website Maintenance</option>
+</select>
+</div>
+</div>
+<div class="col-md-12 ">
+<p class="select-note">What is your projected monthly budget for this project?</p>
+<div class="form-group">
+<input class="form-control" name="price_range" placeholder="$" required type="number">
+</div>
+</div>
+<div class="col-md-12 ">
+<div class="form-group">
+<textarea class="form-control" name="brief" placeholder="Your Idea" rows="8" required></textarea>
+</div>
+</div>
+<div class="row">
+<div class="col-md-12">
+<div class="form-group">
+<button type="submit" class="blue-btn  hvr-bounce-to-right" type="buton">Hear From an Expert</button>
+</div>
+</div>
+</div>
+<input type="hidden" id="lead_area" name="lead_area" value="for $179">
+<input type="hidden" id="lead_org_price" name="lead_org_price" value>
+<input type="hidden" name="send" value="1">
+
+<input type="hidden" name="lb_source" value>
+<input type="hidden" name="lb_source_cat" value>
+<input type="hidden" name="lb_source_nam" value>
+<input type="hidden" name="lb_source_ema" value>
+<input type="hidden" name="lb_source_con" value>
+<input type="hidden" name="lb_source_pho" value>
+<input type="hidden" name="lb_source_off" value>
+
+<input type="hidden" name="fullpageurl" value>
+<input type="hidden" name="pageurl" value>
+
+<input type="hidden" name="ip2loc_ip" value>
+<input type="hidden" name="ip2loc_isp" value>
+<input type="hidden" name="ip2loc_org" value>
+<input type="hidden" name="ip2loc_country" value>
+<input type="hidden" name="ip2loc_region" value>
+<input type="hidden" name="ip2loc_city" value>
+<input type="hidden" name="service_id" value="5731" class="service_id">
+</form>
+</div>
+</div>
+<div class="col-md-5 col-sm-5 col-xs-12">
+<div class="contact-right wow fadeInRight" data-wow-delay="0.6s" data-wow-duration="2s">
+<div class="contact-icon-new">
+<img loading="lazy" src="img/phone-ringing.png" class="img-responsive" alt="img">
+</div>
+<a class="contact-phone" href="tel:(855) 364-3354">(855) 364-3354</a>
+<h3>Ready to speak with our marketing expert? <br> Give us a ring!</h3>
+<div class="call-to-action-margin">
+<div class="flexRow det-box">
+<img loading="lazy" alt="Image" class="img-responsive" data-url="img/contact-icon1.png">
+<h3>1.6M <span>Hours of Expertise</span></h3>
+</div>
+<div class="flexRow det-box">
+<img loading="lazy" alt="Image" class="img-responsive" data-url="img/contact-icon2.png">
+<h3>200+ <span>Digital Marketing Masters On-Staff</span></h3>
+</div>
+<div class="flexRow det-box">
+<img loading="lazy" alt="Image" class="img-responsive" data-url="img/contact-icon3.png">
+<h3>1,128<span> Websites Launched</span></h3>
+</div>
+</div>
+<ul class="social social-new-1 wow zoomIn" data-wow-delay="2s" data-wow-duration="2s">
+<li><a href="https://www.facebook.com/profile.php?id=100092980839774" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+
+<li><a href="https://www.linkedin.com/company/TrustReputationHub/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+
+<li><a href="" target="_blank"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+
+<li><a href="https://www.bark.com/en/us/company/trust-reputation-hub/o1epN/" target="_blank"><img src="img/bark-icon.png"></a> </li>
+
+<li><a href="https://www.sitejabber.com/reviews/TrustReputationHub.com" target="_blank" class="dmca-badge"> <img src="img/sitejabber-icon.png"></a> </li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</section>
+</style>
+<?php include_once('includes/footer.php')?></body>
+</html>
+</div>
